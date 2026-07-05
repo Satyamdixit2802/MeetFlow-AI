43b87269323030269de7ec9c12f753e2ee28ee0f
Satyam Dixit <satyamdixit2003@gmail.com>
2026-07-05 23:29:03 +0530

    create user model

---
+++ web/src/models/User.model.ts
@@ -0,0 +1,58 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+}
+
+const UserSchema: Schema<User> = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      trim: true,
+      
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+     
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+
+    verifyCode: {
+      type: String,
+      required: [true, "Verification code is required"],
+    },
+
+    verifyCodeExpiry: {
+      type: Date,
+      required: [true, "Verification code expiry is required"],
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  }
+  
+);
+
+
+const UserModel: Model<User> =
+  mongoose.models.User || mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
