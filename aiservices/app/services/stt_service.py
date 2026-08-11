1d1d984a0192c55ce7f97fabc9ece002e3b075e2
Satyam Dixit <satyamdixit2003@gmail.com>
2026-08-11 23:12:33 +0530

    Middleware file location changed

--- aiservices/app/services/stt_service.py
+++ aiservices/app/services/stt_service.py
@@ -1,10 +1,13 @@
 from groq import Groq
 from app.config import GROQ_API_KEY
 
+
 client = Groq(api_key=GROQ_API_KEY)
 
 async def transcribe_audio(file_path: str) -> str:
+
      with open(file_path,"rb") as audio_file:
+
         trancription = client.audio.transcriptions.create(
             model= "whisper-large-v3",
             file= audio_file,
